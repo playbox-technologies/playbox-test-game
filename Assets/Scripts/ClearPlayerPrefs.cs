a6771e12e6b02b409515881c7a2ef53414541096
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-09-02 13:43:39 +0300

    add addressables

--- Assets/Scripts/ClearPlayerPrefs.cs
+++ Assets/Scripts/ClearPlayerPrefs.cs
@@ -1,9 +1,14 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
+
 using UnityEngine;
 
 public class ClearPlayerPrefs : MonoBehaviour
 {
+#if UNITY_EDITOR
     [MenuItem("Playbox/Clear PlayerPrefs")]
+#endif
     static void ClearPrefs()
     {
         PlayerPrefs.DeleteAll();
