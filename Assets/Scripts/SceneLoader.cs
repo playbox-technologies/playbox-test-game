ae03ab9fa1fadd50766766f004e8c1d7657cb243
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-09-02 14:09:54 +0300

    remove addressables

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,24 +1,16 @@
-using System;
-using InspectorButton;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
    [SerializeField]
-   private AssetReference loadedScene;
-
-   private void Start()
-   {
-      Debug.Log(loadedScene.SubObjectName);
-   }
+   private string loadedScene;
 
    public void loadScene()
    {
       Debug.Log("loading scene");
       
-      loadedScene.LoadSceneAsync(LoadSceneMode.Single,true);
+      SceneManager.LoadScene(loadedScene);
    }
    
 }
