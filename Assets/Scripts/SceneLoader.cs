f01514ce06fb3c33031fbef7ecd3fa080bbaea53
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-09-03 12:46:31 +0300

    add loadbar

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,32 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneLoader : MonoBehaviour
 {
    [SerializeField]
    private string loadedScene;
 
+   [SerializeField]
+   Slider slider;
+   
+   AsyncOperation asyncOperation;
+   
    public void loadScene()
    {
       Debug.Log("loading scene");
       
-      SceneManager.LoadScene(loadedScene);
+      asyncOperation = SceneManager.LoadSceneAsync(loadedScene);
+      
    }
-   
+
+   private void Update()
+   {
+       if (asyncOperation != null)
+       {
+           slider.value = asyncOperation.progress;
+       }
+   }
 }
