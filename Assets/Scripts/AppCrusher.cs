1c791275c3c52c4a11d71a5e13ebb225e5c8ed33
Sergiychuk Viachaslau <qertysig@gmail.com>
2025-09-06 17:27:00 +0300

    update firebase test

--- Assets/Scripts/AppCrusher.cs
+++ Assets/Scripts/AppCrusher.cs
@@ -1,14 +1,17 @@
+using System.Globalization;
 using Playbox;
 using UnityEngine;
+using Firebase.Analytics;
 
 public class AppCrusher : MonoBehaviour
 {
-   public void verification()
+   public void Kupit()
    {
-       InAppVerification.Validate("test_product_id","test_product_receipot","000", (b) =>
-       {
-           Debug.Log(b);
-           
-       });
+        FirebaseAnalytics.LogEvent(
+                              FirebaseAnalytics.EventPurchase,
+                              new Parameter(FirebaseAnalytics.ParameterTransactionID, "jmz323bz"),
+                              new Parameter(FirebaseAnalytics.ParameterValue, 323.ToString(CultureInfo.InvariantCulture)),
+                              new Parameter(FirebaseAnalytics.ParameterCurrency, "USD")
+                          );
    }
 }
